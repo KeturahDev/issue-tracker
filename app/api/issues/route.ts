90198b08d707d487b8d45ad7c90f91bab24678d8
Keturah Howard <keturah.dev@gmail.com>
2024-03-22 18:20:06 -0700

    create post endpoint for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,24 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validate = schema.safeParse(body);
+
+  if (!validate.success)
+    return NextResponse.json(validate.error.errors, { status: 400 });
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
