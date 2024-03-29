33c892ea780c819a3a98237178c5a3a6d398f486
Keturah Howard <keturah.dev@gmail.com>
2024-03-28 20:07:17 -0700

    style issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,6 +2,8 @@ import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import React from "react";
 import delay from "delay";
+import { Card, Heading, Text } from "@radix-ui/themes";
+import Badge from "@/app/components/Badge";
 
 interface Props {
   params: { id: string };
@@ -15,10 +17,14 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
   if (!issue) notFound();
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.status}</p>
-      <p>{issue.description}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading my="2">{issue.title}</Heading>
+      <div className="flex flex-row space-x-2 align-center mb-5">
+        <Badge status={issue.status} />
+        <Text size="2">{issue.createdAt.toDateString()}</Text>
+      </div>
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
     </div>
   );
 };
