3bc1d76c27bdc7b950dcf50e97cfa8fa19b36a8e
Keturah Howard <keturah.dev@gmail.com>
2024-04-04 11:16:51 -0700

    fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,18 +1,21 @@
 "use client";
-import { Callout, TextField, Button } from "@radix-ui/themes";
-import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { LuAlertCircle } from "react-icons/lu";
+import { z } from "zod";
 import ErrorMessage from "../../components/ErrorMessage";
 import Spinner from "../../components/Spinner";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { issueSchema } from "../../validationSchemas";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
-import { z } from "zod";
-import { Issue } from "@prisma/client";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 export type IssueFormData = z.infer<typeof issueSchema>;
 
@@ -38,6 +41,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         await axios.post("/api/issues", data);
       }
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setSubmitting(false);
       setError("An unexpected error occured.");
