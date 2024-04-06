d6c5b367dfe174b30a6dc639725fdc4b7de3f324
Keturah Howard <keturah.dev@gmail.com>
2024-04-05 20:19:08 -0700

    secure Write, Edit and Delete functionality

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,11 +4,15 @@ import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 interface Props {
   params: { id: string };
 }
 const IssueDetailsPage = async ({ params: { id } }: Props) => {
+  const session = await getServerSession(authOptions);
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
   });
@@ -19,12 +23,14 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex gap="2" direction="column">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session ? (
+        <Box>
+          <Flex gap="2" direction="column">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      ) : null}
     </Grid>
   );
 };
